e8d2537419d46182bfb3d88a44e4f8c892907940
Sholom Jacks <sholomjacks504@gmail.com>
2020-03-31 21:49:16 -0400

    made a navigation bar

--- src/games.js
+++ src/games.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import './games.css'
 
-export default class Games extends React.Component{
+export default class Games extends React.Component {
+
+    state = {
+        activeGames: "",
+        activeAdmin: "",
+        activeHome: ""
+    }
+
     render() {
-        return(
-            <>
-            <h1>Games</h1>
-            <br/>
-            <h2>
-                Games will be coming soon....<br/>
-                for now you can join roblox <a href="https://www.roblox.com" rel="noopener noreferrer" target="_blank" id="black">here!</a>
-            </h2>
-            </>
+
+        switch (window.location) {
+            case '/admin':
+                if (this.state.activeAdmin !== "active") this.setState({ activeAdmin: "active" })
+                break;
+
+            case '/games':
+                if (this.state.activeGames !== "active") this.setState({ activeGames: "active" })
+                break;
+
+            default:
+                if (this.state.activeHome !== "active") this.setState({ activeHome: "active" })
+                break;
+        }
+
+        return (
+            <main class="main">
+                <aside class="sidebar">
+                    <nav class="nav">
+                        <ul>
+                            <li class={this.state.activeAdmin}><a href="/admin">Admin Commands</a></li>
+                            <li class={this.state.activeGames}><a href="/games">Games</a></li>
+                            <li class={this.state.activeGames}><a href="/home">Home</a></li>
+                        </ul>
+                    </nav>
+                </aside>
+                <h1>Games</h1>
+                <br />
+                <h2>
+                    Games will be coming soon....<br />
+                for now you can join roblox <a href="https://www.roblox.com" rel="noopener noreferrer" target="_blank" id="bk">here!</a>
+                </h2>
+            </main>
         )
     }
 }
--- src/users.js
+++ src/users.js
@@ -1,21 +1,55 @@
 import React from 'react'
 import './home.css'
-function go(location) {
-  var hash = localStorage.getItem("hash")
-  if (hash !== null) {
-    window.location = location
-  }
-}
+// function go(location) {
+//     var hash = localStorage.getItem("hash")
+//     if (hash !== null) {
+//         window.location = location
+//     }
+// }
 
 class Users extends React.Component {
-  render() {
-    return <div>
-      <h1>Welcome, User!</h1>
-      <h2>Commands</h2>
-      <img id="firstlc" src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/terminal-512.png" alt="Commands" height="50" onClick={() => go('/admin')} />
-      <h2>Games</h2>
-      <img id="firstlc" src="https://www.vippng.com/png/full/355-3554420_gamer-profile-png-icon-font-awesome-game-icon.png" alt="Games" height="50" onClick={() => go('/games')} />
-    </div>
-  }
+
+    state = {
+        activeGames: "",
+        activeAdmin: "",
+        activeHome: ""
+    }
+
+    render() {
+
+        switch (window.location) {
+            case '/admin':
+                if(this.state.activeAdmin !== "active") this.setState({activeAdmin: "active"})
+            break;
+            
+            case '/games': 
+            if(this.state.activeGames !== "active") this.setState({activeGames: "active"})
+            break;
+
+            default:
+                if(this.state.activeHome !== "active") this.setState({activeHome: "active"})
+            break;
+        }
+
+        return (
+            <main class="main">
+                <aside class="sidebar">
+                    <nav class="nav">
+                        <ul>
+                            <li class={this.state.activeAdmin}><a href="/admin">Admin Commands</a></li>
+                            <li class={this.state.activeGames}><a href="/games">Games</a></li>
+                            <li class={this.state.activeGames}><a href="/home">Home</a></li>
+                        </ul>
+                    </nav>
+                </aside>
+
+                <section class="twitter">
+                    <div class="container">
+                        <h1 id="high">Welcome, User!</h1>
+                    </div>
+                </section>
+            </main>
+        )
+    }
 }
 export default Users
