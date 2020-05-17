7bf6d4080164c1c850c8adb680a5d138f441c427
Benny <binyomin3@gmail.com>
2020-05-17 12:31:54 -0400

    style

---
+++ lab/src/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() { 
+        return (
+            <center>
+                <h1>Welcome To The Lab!</h1>
+
+                <h2>Here you will be able to express your hacking skills</h2>
+            </center>
+        );
+    }
+}
+ 
+export default Home;
