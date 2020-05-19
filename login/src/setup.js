a33783441c3f615416a9b3894489f793d43f77bc
Benny <binyomin3@gmail.com>
2020-05-18 20:00:50 -0400

    problem

--- login/src/setup.js
+++ login/src/setup.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Setup extends Component {
     render() { 
         return (
-            <center>
-                <h1>Setup</h1>
-                       );
+            <center>   
+            <h1>Setup</h1>
+            </center>
+                
+                    );
     }
 }
  
