aebc3eab95f8d9cf018d483c1dbc44eb0917fce8
Sholom <sholomjacks504@gmail.com>
2020-05-20 16:26:23 -0400

    MultiSelect dropdown added

--- login/src/setup.js
+++ login/src/setup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Checkbox } from 'primereact/checkbox'
 import { InputText } from 'primereact/inputtext'
 import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
 
 const typeSelectItems = [
     { label: "Informational Websites" },
@@ -15,12 +16,19 @@ const typeSelectItems = [
     { label: "Portfolio" }
 ];
 
+const pageSelectItems = [
+    { label: "1" },
+    { label: "2" },
+    { label: "3" }
+]
+
 class Setup extends Component {
 
     state = {
         domainChecked: false,
         domain: "",
         type: "",
+        pages: []
     }
 
     render() {
@@ -38,6 +46,15 @@ class Setup extends Component {
                 {this.state.domainChecked ? <> <InputText value={this.state.domain} placeholder="Domain" onChange={(e) => this.setState({ domain: e.target.value })} /> <br/> <br/> </> : null}
 
                 <Dropdown value={this.state.type} options={typeSelectItems} onChange={(e) => {this.setState({ type: e.value })}} placeholder="Select a type of website"/>
+
+                <br/>
+                <br/>
+
+                <h2>Choose the pages you would like on your website:</h2>
+
+                <br/>
+
+                <MultiSelect style={{ minWidth: '12em' }} filter={true} filterPlaceholder="Search" value={this.state.pages} options={pageSelectItems} placeholder="Pages" onChange={(e) => this.setState({ pages: e.value })} />
 
             </center>
         );
